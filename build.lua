dd8fd47fddae45d76f1924d89ae4b3466d58354f
Frank Mittelbach <frank.mittelbach@latex-project.org>
2018-11-03 00:48:16 +0100

    simple l3build setup added

---
+++ build.lua
@@ -0,0 +1,19 @@
+#!/usr/bin/env texlua
+
+--[[
+     For documentation and usage of the l3build system
+     see l3build.pdf
+--]]
+
+module = "lipsum"
+
+typesetfiles  = {"*.tex"}
+
+checkruns     = 1
+
+
+if not release_date then
+   kpse.set_program_name("kpsewhich")
+   dofile(kpse.lookup("l3build.lua"))
+end
+
